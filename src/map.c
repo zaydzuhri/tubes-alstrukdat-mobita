5ac464daad24d7cf87827de5b19e1bc6f270b2fb
DaffaRomyz <daffaromyzaufa@gmail.com>
2021-11-16 00:02:15 +0700

    revisi map

--- src/map.c
+++ src/map.c
@@ -9,6 +9,7 @@ void CreateMap(int mapRows, int mapCols, Matrix *M) {
     /* KAMUS */
     int i,j;
     /* Algoritma */
+
     CreateMatrix(mapRows+2, mapCols+2, M);
     ROWS(*M) = mapRows+2;
     COLS(*M) = mapCols+2;
@@ -25,8 +26,26 @@ void CreateMap(int mapRows, int mapCols, Matrix *M) {
     }
 };
 
-void displayMap(int mapRows, int mapCols, ListLoc locL, Location curLoc, Matrix adjM, ToDoList todoL, InProgressList progL) {
-    /*  I.S. mapRows, mapCols, time, locL, curLoc, adjM, todoL, progL terdefinisi.
+boolean isInTodolist(ToDoList L, char name) {
+    /*  mencari apakah pesanan name ada di L*/
+    /* KAMUS */
+    Address p;
+    boolean found;
+    /* ALGORITMA */
+    found = false;
+    p = FIRST(L);
+    while (p != NULL && !found) {
+        if (LOC_NAME(PICK_UP_LOCATION(INFO(p))) == name) {
+            found = true;
+        }
+        p = NEXT(p);
+    }
+    return found;
+};
+
+
+void displayMap(int mapRows, int mapCols, ListLoc locL, Location curLoc, Matrix adjM, ToDoList todoL, Bag bag) {
+    /*  I.S. mapRows, mapCols, locL, curLoc, adjM, todoL, progL terdefinisi.
     F.S. Menuliskan peta  */
     /* KAMUS */
     Matrix M;
@@ -34,43 +53,34 @@ void displayMap(int mapRows, int mapCols, ListLoc locL, Location curLoc, Matrix 
     ListLoc adjLocList;
     Address p;
     /* Algoritma */
-
+    
     CreateMap(mapRows, mapCols, &M);
     CreateListLoc(&adjLocList, 30);
     getAdjacentLocations(adjM, locL, &adjLocList, curLoc);
-
+    
     for (i = 0; i < NEFF(locL); i++) {
-        ELMT(M, 1+LOC_ROW(LOC(locL,i)),1+LOC_COL(LOC(locL,i))) = (int) LOC_NAME(LOC(locL,i));
+        ELMT(M, LOC_ROW(LOC(locL,i)),LOC_COL(LOC(locL,i))) = (int) LOC_NAME(LOC(locL,i));
     }
     
     for (i = 0; i < ROWS(M); i++) {
         for (j = 0; j < COLS(M); j++) {
-            // print hijau untuk destination dari current location
-            for (k = 0; k < NEFF(adjLocList); k++) {
-                if (ELMT(M,i,j) == (int) LOC_NAME(LOC(adjLocList,k))) {
-                    print_green((char) ELMT(M,i,j));
-                }
-            }
 
-            // print merah untuk tempat pickup location
-            p = FIRST(todoL);
-            while (p != NULL) {
-                if ((int) LOC_NAME(PICK_UP_LOCATION(INFO(p))) == ELMT(M,i,j)) {
-                    print_red((char) ELMT(M,i,j));
-                }
-            }
-
-            // print orange untuk current location
             if (ELMT(M,i,j) == (int) LOC_NAME(curLoc)) {
                 print_yellow((char) ELMT(M,i,j));
             }
 
-            // print biru untuk tempat drop off item teratas
-            else if ((int) LOC_NAME(DROP_OFF_LOCATION(INFO(FIRST(progL)))) == ELMT(M,i,j)) {
+            else if (isInTodolist(todoL, (char) ELMT(M,i,j))) {
+                print_red((char) ELMT(M,i,j));
+            }
+
+            else if (!isBagEmpty(bag) && (int) LOC_NAME(DROP_OFF_LOCATION(TOP(bag))) == ELMT(M,i,j)) {
                 print_blue((char) ELMT(M,i,j));
             }
 
-            // print hitam untuk lainya
+            else if (isInListLoc(adjLocList, (char) ELMT(M,i,j))){
+                print_green((char) ELMT(M,i,j));
+            }
+
             else {
                 printf("%c", (char) ELMT(M,i,j));
             }
--- src/map.h
+++ src/map.h
@@ -8,13 +8,17 @@
 #include "pcolor.h"
 #include "inprogresslist.h"
 #include "listdinloc.h"
+#include "bag.h"
 
 void CreateMap(int mapRows, int mapCols, Matrix *M);
     /*  I.S. mapRows, mapCols, locL terdefinisi
         F.S. M membuat matrix kosong dengan batas 42 => (int) '*' dan berisi 32 => (int) ' ' */ 
 
-void displayMap(int mapRows, int mapCols, int time, ListLoc locL, Location curLoc, Matrix adjM, ToDoList todoL, InProgressList progL);
+void displayMap(int mapRows, int mapCols, ListLoc locL, Location curLoc, Matrix adjM, ToDoList todoL, Bag bag);
     /*  I.S. mapRows, mapCols, time, locL, curLoc, adjM, todoL, progL terdefinisi.
         F.S. Menuliskan peta  */
+
+boolean isInTodolist(ToDoList L, char name);
+    /*  mencari apakah pesanan name ada di L*/
 
 #endif
