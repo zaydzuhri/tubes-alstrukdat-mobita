920a20a7306f5dd3bd99d503f18cbdcad3023583
hcarissa <13520164@std.stei.itb.ac.id>
2021-11-15 23:17:50 +0700

    add main menu

--- src/main.c
+++ src/main.c
@@ -13,10 +13,12 @@
 #include "wordmachine.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define input currentWord.contents
 
 int main() {
+    /*KAMUS*/
     ListLoc locList;
     Matrix adjMatrix;
     DaftarPesanan dafPesananBefore;
@@ -36,22 +38,79 @@ int main() {
 
     int speedBoostDur = 0;
 
-    boolean gameLoop = true;
+    boolean gameLoop = false;
 
     Location currentLoc;
 
-    CreateListLoc(&locList, 30);
-    createDaftarPesanan(&dafPesananBefore);
-    createDaftarPesanan(&dafPesananAfter);
-    CreateBag(&bag);
-    createToDoList(&toDo);
-    createInProgressList(&inProgress);
-    CreateInventory(&inventory);
+    /*ALGORITMA*/
+    printf("           Selamat datang di permainan\n");
+    printf("_______________       _____________________________ \n");
+    printf("__  ____/_  __ \\      ___  __ )___  _/__  __/__    |\n");
+    printf("_  / __ _  / / /________  __  |__  / __  /  __  /| |\n");
+    printf("/ /_/ / / /_/ /_/_____/  /_/ /__/ /  _  /   _  ___ |\n");
+    printf("\\____/  \\____/        /_____/ /___/  /_/    /_/  |_|\n\n");
+    printf("           Bantu Mobita mengantarkan\n        pesanan-pesanan usaha orang tuanya!\n");
+    printf("===================================================\n");
+    printf("||                 MENU UTAMA                    ||\n");
+    printf("||                                               ||\n");
+    printf("||                1. NEW_GAME                    ||\n");
+    printf("||                  2. EXIT                      ||\n");
+    printf("||                                               ||\n");
+    printf("===================================================\n");
+    printf("ENTER COMMAND: ");
+    startWord();
+    while(!isSameString(currentWord, "NEW_GAME") && !isSameString(currentWord, "EXIT")){
+        printf("Command not recognized!\nENTER NEW COMMAND: ");
+        startWord();
+    }
+    if (isSameString(currentWord, "NEW_GAME")){
+        printf("Masukkan nama file konfigurasi level (jangan lupa \".txt\") : ");
+        startWord();
+        while(!(access(currentWord.contents, F_OK)!=-1)){
+            printf("Nama file tidak valid!\n");
+            printf("Masukkan kembali nama file konfigurasi level (jangan lupa \".txt\") : ");
+            startWord();
+        }
+        CreateListLoc(&locList, 30);
+        createDaftarPesanan(&dafPesananBefore);
+        createDaftarPesanan(&dafPesananAfter);
+        CreateBag(&bag);
+        createToDoList(&toDo);
+        createInProgressList(&inProgress);
+        CreateInventory(&inventory);
 
-    readConfig("config.txt", &locList, &mapRows, &mapCols, &adjMatrix, &dafPesananBefore);
+        readConfig("config.txt", &locList, &mapRows, &mapCols, &adjMatrix, &dafPesananBefore);
 
-    getLocationFromList(locList, &currentLoc, '8');
-
+        getLocationFromList(locList, &currentLoc, '8');
+        gameLoop = true;
+        printf("Loading");
+        fflush(stdout);
+        sleep(1);
+        printf(".");
+        fflush(stdout);
+        sleep(1);
+        printf(".");
+        fflush(stdout);
+        sleep(1);
+        printf(".\n");
+        printf("Loading success!\n");
+        sleep(1);
+        printf("Selamat membantu Mobita mengantarkan pesanan!\n");
+        sleep(1);
+        printf("              ___\n");
+        printf("             /  _\\\n");
+        printf("             | /\\_|\n");
+        printf("          __-'' _'\n");
+        printf("         ----'-.\n");
+        printf("            |#\\#)_,_\n");
+        printf("            )##\\__ _\\__.-. \n");
+        printf("    -  .-  (###)   '---.  `.\n");
+        printf(" -   __\\____`.#\\(      )  L(|\n");
+        printf("   .'__//\\    \\#)`-._.' / \\\\==.\n");
+        printf("  /_/_//\\_\\_  /#/  ### / //\\\\ \\\n");
+        printf("   |(________(##)___/-' '| (_) |\n");
+        printf("____\\___/_________________\\___/________________________________________\n");
+    }
     while (gameLoop) {
         printf("\nWaktu: %d\n", time);
         printf("ENTER COMMAND: ");
