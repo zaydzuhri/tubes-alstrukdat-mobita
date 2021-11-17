bad5ec053a1a33495b996d42e912be02a95f5810
DaffaRomyz <daffaromyzaufa@gmail.com>
2021-11-18 01:23:56 +0700

    mengubah warna background menjadi WHITE

--- src/pcolor.c
+++ src/pcolor.c
@@ -10,30 +10,43 @@ Created at: 17/10/2019
 
 void print_red(char c) {
     printf("%s%c", RED, c);
-    printf("%s", NORMAL);
+    printf("%s", WHITE);
 }
 
 void print_green(char c) {
     printf("%s%c", GREEN, c);
-    printf("%s", NORMAL);
+    printf("%s", WHITE);
 }
 
 void print_yellow(char c) {
     printf("%s%c", YELLOW, c);
-    printf("%s", NORMAL);
+    printf("%s", WHITE);
 }
 
 void print_blue(char c) {
     printf("%s%c", BLUE, c);
-    printf("%s", NORMAL);
+    printf("%s", WHITE);
 }
 
 void print_magenta(char c) {
     printf("%s%c", MAGENTA, c);
-    printf("%s", NORMAL);
+    printf("%s", WHITE);
 }
 
 void print_cyan(char c) {
     printf("%s%c", MAGENTA, c);
-    printf("%s", NORMAL);
+    printf("%s", WHITE);
+}
+
+void print_black(char c) {
+    printf("%s%c", BLACK, c);
+    printf("%s", WHITE);
+}
+
+void print_white(char c) {
+    printf("%s%c", WHITE, c);
+    printf("%s", WHITE);
+}
+void print_normal() {
+    printf("%s", BLACK);
 }
